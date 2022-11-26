dc98b9887219f1225e9fa0f73fd789aff9983ba9
Kiz03 <115060073+Kiz03@users.noreply.github.com>
2022-11-25 23:55:29 -0500

    Update reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let e = ([...str]);
+    let s = e.reverse();
+    let g = s.join('');                    
+    let f = g.toString()
+    return(f);
 };
 
 // Do not edit below this line
