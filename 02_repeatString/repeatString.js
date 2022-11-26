c274a2659b4de82a77251d85ea56d695f3d84526
Kiz03 <115060073+Kiz03@users.noreply.github.com>
2022-11-25 19:25:06 -0500

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
  const repeatString = function(string, num) {
         let i;
+  if (num<0) {
+   return 'ERROR';
+  }
         let result = '';
       for (i = 0; i<num; i++) {
         result+=string;
