3146fa018d33157acda58594c38800d83727721c
Kiz03 <115060073+Kiz03@users.noreply.github.com>
2022-11-27 22:30:09 -0500

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,7 @@
 const removeFromArray = function(array, rem) {
-  let e = array.toString();
-  let b = rem.toString();
-  let v = e.replaceAll(b, '');
-  let c = v.replaceAll(',', '')
-  let o = ([...c]);
+ return array != rem;
 
-
-  return o;
+};
 
 
 };
