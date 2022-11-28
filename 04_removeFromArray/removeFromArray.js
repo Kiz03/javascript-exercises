8f6def33ce16122d1b1bfbb23a553e3bc47b4ade
Kiz03 <115060073+Kiz03@users.noreply.github.com>
2022-11-27 21:33:02 -0500

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, rem) {
+  let e = array.toString();
+  let b = rem.toString();
+  let v = e.replaceAll(b, '');
+  let c = v.replaceAll(',', '')
+  let o = ([...c]);
+
+
+  return o;
+
 
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
