ab56528c51ea39079b4a3b14cb1404e1fe61d2ec
Kiz03 <115060073+Kiz03@users.noreply.github.com>
2022-11-28 23:59:49 -0500

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,25 @@
 const sumAll = function(frs, end) {
-  
   let sum = 0;
   let i = frs;
-  while (i<= end) {
+  let c = end;
+  if (i>c) {
+    i = end;
+    c = frs;
+  } if ( i<0 || c<0 ) {
+    return 'ERROR';
+    return;
+  } if (Number.isInteger(i) !== true || Number.isInteger(c) !== true ) {
+    return 'ERROR';
+    return;
+  }
+  while (i<= c) {
     sum+=i;
     i++
   }
   return sum;
+
+
+};
 
 
 };
