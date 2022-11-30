4c6f2a8d619388add6ddd29f68487cb8df5db42d
Kiz03 <115060073+Kiz03@users.noreply.github.com>
2022-11-30 13:46:07 -0500

    Update leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,13 +2,15 @@ const leapYears = function(year) {
 
   if (year % 100 === 0) {
     if (year === 1600 || year === 2000) {
-      return true; 
+      return (true); 
     } else {
-      return false;;
+      return (false); 
     }
     
   } else if (year % 4 === 0) {
-    return true; 
+    return (true); 
+  } else {
+    return (false);
   }
    
   
