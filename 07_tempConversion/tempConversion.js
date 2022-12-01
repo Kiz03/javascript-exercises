c53cf22c27b350f1fed4d868a4f50a20a88c3ae0
Kiz03 <115060073+Kiz03@users.noreply.github.com>
2022-12-01 01:15:53 -0500

    Update tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
 const convertToCelsius = function(fer) {
   let cel = 5/9 * (fer - 32);
-  return cel.toFixed(1); 
+  if (Number.isInteger(cel) === true) {
+    return cel;
+  } else {
+    return cel.toFixed(1); 
+
+  }
+  
 };
 
 const convertToFahrenheit = function(cer) {
   let fel = 9/5 * (cer) + 32;
-  return fel.toFixed(1);
+  if (Number.isInteger(fel) === true) {
+    return fel;
+  } else {
+    return fel.toFixed(1);
+
+  }
+  
 };
 
 // Do not edit below this line
