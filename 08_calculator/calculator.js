767f1cb9c471d1f8700b238fdae60b06a69b5a1c
Kiz03 <115060073+Kiz03@users.noreply.github.com>
2023-01-05 20:41:53 -0500

    Update calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,57 @@
-const add = function() {
+const add = function(a, b) {
+    return a + b;
 	
 };
 
-const subtract = function() {
+
+const subtract = function(a, b) {
+    return a - b;
 	
 };
 
-const sum = function() {
+
+const sum = function(su) {
+    let wrk = Array.from(su);
+    total = 0;
+    for (let i = 0; i<wrk.length; i++) {
+        total += wrk[i];
+    }
+    return total;
+    };
+
+
+
+const multiply = function(su) {
+    let wrk = Array.from(su);
+    total = 1;
+    for (let i = 0; i<wrk.length; i++) {
+        total *= wrk[i];
+    }
+    return total;
+
+
+};
+
+
+
+const power = function(a, b) {
+    return a ** b;
 	
 };
 
-const multiply = function() {
 
-};
 
-const power = function() {
+const factorial = function(fck) {
+    total = 1;
+    for (let i = 1; i<=fck; i++) {
+        total *= i;
+    }
+
+    return total;
 	
 };
 
-const factorial = function() {
-	
-};
+
 
 // Do not edit below this line
 module.exports = {
